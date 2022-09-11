3f9523f9c0b9a3f06de308bb2de332274ddc6be1
Roberta Nascimento <71949339+betavasconcellos@users.noreply.github.com>
2022-09-11 15:36:13 -0300

    aula 56

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
 import './assets/global.scss';
-import Header from './assets/components/Header';
+import styles from './App.module.scss'
+
+import { Header } from './assets/components/Header';
+import { Plus, Trash, Check } from 'phosphor-react'
 
 function App() {
   return (
-  <Header />
+    <>
+      <Header />
+      <main className={styles.container}>
+        <form className={styles.addTaskForm}>
+          <input type="text" placeholder="Insira uma nova atividade" />
+          <button><Plus /></button>
+        </form>
+
+        <h3 className={styles.status}>Tarefas Concluídas <span>3 de 6</span></h3>
+
+        <ul className={styles.taskList}>
+          <li>
+            <label>
+              <input type="checkbox" />
+              <span>
+                <Check />
+              </span>
+            </label>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, aspernatur. Eaque debitis quo tempore deserunt natus dolorum eligendi, nam recusandae soluta veritatis aut illum, sit doloribus sunt temporibus, dolor repellat.</p>
+
+            <button>
+              <Trash />
+            </button>
+          </li>
+        </ul>
+      </main>
+
+
+    </>
   )
 }
 
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,12 +2,10 @@ import styles from './Header.module.scss';
 
 import logoSvg from '../logo.svg';
 
-function Header() {
+export function Header() {
   return (
   <header className={styles.header}>
     <img src={logoSvg} alt="AfroToDo" />
   </header>
   );
 }
-
-export default Header;
